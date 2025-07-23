0557a4ce92347f8a4b94c4676cf3d81ee49a7f8c
Darwin <me@darwinchow.com>
2025-07-23 18:02:31 +0800

    feat: add LiveURL multi-source support

--- functions/[channelId].js
+++ functions/[channelId].js
@@ -11,10 +11,11 @@ export async function onRequest({ request, params, env }) {
   let currentTime = Date.now();
   let currentTimeString = currentTime.toString();
 
-  if (geo.regionCode !== 'CN-CQ' && currentTime - cacheData.liveUrl?.timestamp < env.CACHE_DURATION) {
-    redirectUrl = cacheData.liveUrl?.url.replace(/^(https?:\/\/[^\/]+)/, env.PROXYAPI_LIVE_URL);
+  let liveUrlNoCqcu = getLiveUrl(cacheData.liveUrl, ['tencent', 'baidu'], currentTime);
+  if (geo.regionCode !== 'CN-CQ' && liveUrlNoCqcu !== null) {
+    redirectUrl = getProxyLiveUrl(liveUrlNoCqcu);
   } else {
-    if (!cacheData.playUrl || currentTime - cacheData.playUrl?.timestamp > env.CACHE_DURATION) {
+    if (!cacheData.playUrl || currentTime > cacheData.playUrl?.expires) {
       // Cache is expired, get new data
       let requestBody = {
         cityId: '5A',
@@ -46,14 +47,10 @@ export async function onRequest({ request, params, env }) {
       );
       let playResponse = await fetch(playRequest);
 
-      if (playResponse.status === 200) {
-        cacheData.playUrl = {
-          url: (await playResponse.json()).data.result.protocol[0].transcode[0].url,
-          timestamp: currentTime,
-        };
-
+      let playUrl = (await playResponse.json()).data?.result?.protocol[0]?.transcode[0]?.url;
+      if (playUrl) {
+        setPlayUrl(cacheData, playUrl);
         cacheDataUpdatedTag = true;
-
       } else {
         return errorResponse(playResponse.status, playResponse.statusText);
       }
@@ -63,7 +60,7 @@ export async function onRequest({ request, params, env }) {
       redirectUrl = cacheData.playUrl.url;
     } else {
       let playRequest = new Request(
-        cacheData.playUrl.url.replace(/^(https?:\/\/[^\/]+)/, env.PROXYAPI_URL),
+        getProxyPlayUrl(cacheData.playUrl.url),
         {
           method: 'GET',
           redirect: 'manual',
@@ -75,14 +72,10 @@ export async function onRequest({ request, params, env }) {
       let playResponse = await fetch(playRequest);
 
       if (playResponse.headers.has('Location')) {
-        cacheData.liveUrl = {
-          url: playResponse.headers.get('Location'),
-          timestamp: cacheData.playUrl.timestamp,
-        };
-
+        setLiveUrl(cacheData, playResponse.headers.get('Location'));
         cacheDataUpdatedTag = true;
 
-        redirectUrl = cacheData.liveUrl.url.replace(/^(https?:\/\/[^\/]+)/, env.PROXYAPI_LIVE_URL);
+        redirectUrl = getProxyLiveUrl(playResponse.headers.get('Location'));
       } else {
         return errorResponse(playResponse.status, playResponse.statusText);
       }
@@ -102,6 +95,70 @@ export async function onRequest({ request, params, env }) {
       },
     }
   );
+}
+
+function setPlayUrl(cacheData, url) {
+  let playUrlObject = new URL(url);
+  cacheData.playUrl = {
+    url: url,
+    expires: playUrlObject.searchParams.get('expires') * 1000 || Date.now() + 3600 * 1000,
+    timestamp: Date.now(),
+  };
+}
+
+function getProxyPlayUrl(url) {
+  let playUrlObject = new URL(url);
+  return playUrlObject.href.replace(playUrlObject.origin, env.PROXYAPI_URL);
+}
+
+function setLiveUrl(cacheData, url) {
+  let liveUrlObject = new URL(url);
+
+  if (!cacheData.liveUrl) {
+    cacheData.liveUrl = {};
+  }
+
+  if (liveUrlObject.hostname.startsWith('tencent')) {
+    cacheData.liveUrl.tencent = {
+      url: url,
+      expires: liveUrlObject.searchParams.get('t') * 1000 || Date.now() + 3600 * 1000,
+      timestamp: Date.now(),
+    };
+  } else if (liveUrlObject.hostname.startsWith('baidu')) {
+    cacheData.liveUrl.baidu = {
+      url: url,
+      expires: liveUrlObject.searchParams.get('timestamp') * 1000 || Date.now() + 3600 * 1000,
+      timestamp: Date.now(),
+    };
+  } else if (liveUrlObject.hostname.startsWith('cqcu')) {
+    cacheData.liveUrl.cqcu = {
+      url: url,
+      expires: Date.now() + 1800 * 1000,
+      timestamp: Date.now(),
+    };
+  }
+}
+
+function getLiveUrl(liveUrlObject, sources = ['tencent', 'baidu', 'cqcu'], timestamp = Date.now()) {
+  for (const source of sources) {
+    if (timestamp < liveUrlObject?.[source]?.expires) {
+      return liveUrlObject[source].url;
+    }
+  }
+  return null;
+}
+
+function getProxyLiveUrl(liveUrl) {
+  let liveUrlObject = new URL(liveUrl);
+  let proxyLiveUrl = liveUrl;
+  if (liveUrlObject.hostname.startsWith('tencent')) {
+    proxyLiveUrl = liveUrlObject.href.replace(liveUrlObject.origin, env.PROXYAPI_LIVE_TENCENT_URL);
+  } else if (liveUrlObject.hostname.startsWith('baidu')) {
+    proxyLiveUrl = liveUrlObject.href.replace(liveUrlObject.origin, env.PROXYAPI_LIVE_BAIDU_URL);
+  } else if (liveUrlObject.hostname.startsWith('cqcu')) {
+    proxyLiveUrl = liveUrlObject.href.replace(liveUrlObject.origin, env.PROXYAPI_LIVE_CQCU_URL);
+  }
+  return proxyLiveUrl;
 }
 
 function errorResponse(statusCode, message) {
